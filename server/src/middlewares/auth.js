ff5412ec5499236b82749df9886f715480371cbc
abhijithwebaxium <abhijith.sd@webaxium.com>
2026-01-19 12:45:24 +0530

    console check

--- server/src/middlewares/auth.js
+++ server/src/middlewares/auth.js
@@ -15,7 +15,7 @@ export const protect = async (req, res, next) => {
     ) {
       token = req.headers.authorization.split(" ")[1];
     }
-
+console.log(token, "token check")
     if (!token) {
       req.user = { isAuthenticated: false };
       return next(); // Continue without blocking
